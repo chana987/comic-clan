dd14442aa4e618c87d184ed1ddfd28b9dde5e011
chana987 <chana987@gmail.com>
2020-02-06 13:56:10 +0200

    Updates

---
+++ precache-manifest.91ad89b6450bda9757f13426617bd867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6630434ca51aafa51f7edcfa150b7b",
+    "url": "/comic-clan/index.html"
+  },
+  {
+    "revision": "5d4ca3fd791e68f99314",
+    "url": "/comic-clan/static/css/main.0d1bcda1.chunk.css"
+  },
+  {
+    "revision": "05aa0b9115fc518d69c5",
+    "url": "/comic-clan/static/js/2.f0491468.chunk.js"
+  },
+  {
+    "revision": "6a984828c7bfd37325840f267d05c6ba",
+    "url": "/comic-clan/static/js/2.f0491468.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d4ca3fd791e68f99314",
+    "url": "/comic-clan/static/js/main.064ac5e9.chunk.js"
+  },
+  {
+    "revision": "4f9d3778c7fbd5a48a94",
+    "url": "/comic-clan/static/js/runtime-main.8ad616e9.js"
+  }
+]);
